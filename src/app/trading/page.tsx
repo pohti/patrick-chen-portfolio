22e40b3324f4773716bafc5deb2d08834845f05e
Min Marn Oo <minmarnoo@gmail.com>
2025-06-07 14:45:59 +0800

    chore: setup grid layout

--- src/app/trading/page.tsx
+++ src/app/trading/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
 import React from 'react'
+import GridLayout from "react-grid-layout";
 import "./styles.css"
+import "react-grid-layout/css/styles.css"
+import "react-resizable/css/styles.css"
+
 
 const Trading = () => {
+      const layout = [
+      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 }
+    ];
   return (
-    <div>
-      <div className="top-section">
+    <GridLayout
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1200}
+    >
+      <div key="a">a</div>
+      <div key="b">b</div>
+      <div key="c">c</div>
+      {/* <div className="top-section">
         <div className="chart-container">
           chart
         </div>
@@ -19,8 +38,8 @@ const Trading = () => {
         <div className="trade-tool-container">
           trade tool
         </div>
-      </div>
-    </div>
+      </div> */}
+    </GridLayout>
   )
 }
 
