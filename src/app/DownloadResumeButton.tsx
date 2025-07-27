c211400c2ef51563c732c888e72f2edb2e7beb86
Patrick Chen <minmarnoo@gmail.com>
2025-07-27 14:05:58 +0800

    chore: add actual google drive file id for resume download

--- src/app/DownloadResumeButton.tsx
+++ src/app/DownloadResumeButton.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import './styles.css';
 
+const GOOGLE_DRIVE_FILE_ID = '1sZ7xoBjGqVsgLHrSNAgg6Fi45D944smi';
+const DOWNLOAD_URL = `https://drive.google.com/uc?export=download&id=${GOOGLE_DRIVE_FILE_ID}`;
+
 const DownloadResumeButton = () => {
+  const handleDownload = () => {
+    window.open(DOWNLOAD_URL, '_blank');
+  };
+
   return (
-    <button
-      onClick={() => console.log('download resume')}
-      className="download-resume-button"
-    >
+    <button onClick={handleDownload} className="download-resume-button">
       Download Resume
     </button>
   );
