271b05255e27a158b6706877af13d8deacde7671
Patrick Chen <minmarnoo@gmail.com>
2025-09-12 13:22:21 +0200

    chore: update start date of chart data

--- src/store/instrumentHistory/index.ts
+++ src/store/instrumentHistory/index.ts
@@ -32,16 +32,16 @@ const today = new Date();
 const endDate = today.toISOString().slice(0, 10);
 
 export const chartData: Record<string, AreaData[]> = {
-  AAPL: generateStockData('2024-01-01', endDate, 170),
-  GOOGL: generateStockData('2024-01-01', endDate, 130),
-  VOO: generateStockData('2024-01-01', endDate, 400),
-  QQQ: generateStockData('2024-01-01', endDate, 350),
-  SPX: generateStockData('2024-01-01', endDate, 4500),
-  IXIC: generateStockData('2024-01-01', endDate, 14000),
-  NVQ: generateStockData('2024-01-01', endDate, 100),
-  SCHH: generateStockData('2024-01-01', endDate, 70),
-  VFIAX: generateStockData('2024-01-01', endDate, 400),
-  FXAIX: generateStockData('2024-01-01', endDate, 150),
-  VNQ: generateStockData('2024-01-01', endDate, 80),
+  AAPL: generateStockData('2022-01-01', endDate, 170),
+  GOOGL: generateStockData('2022-01-01', endDate, 130),
+  VOO: generateStockData('2022-01-01', endDate, 400),
+  QQQ: generateStockData('2022-01-01', endDate, 350),
+  SPX: generateStockData('2022-01-01', endDate, 4500),
+  IXIC: generateStockData('2022-01-01', endDate, 14000),
+  NVQ: generateStockData('2022-01-01', endDate, 100),
+  SCHH: generateStockData('2022-01-01', endDate, 70),
+  VFIAX: generateStockData('2022-01-01', endDate, 400),
+  FXAIX: generateStockData('2022-01-01', endDate, 150),
+  VNQ: generateStockData('2022-01-01', endDate, 80),
   // ...add more symbols as needed
 };
