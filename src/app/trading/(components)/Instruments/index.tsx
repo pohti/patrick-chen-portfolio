f80764a02f0256a439834c7d3da3ace12acf1925
Patrick Chen <minmarnoo@gmail.com>
2025-10-05 20:47:05 +0200

    refactor: import

--- src/app/trading/(components)/Instruments/index.tsx
+++ src/app/trading/(components)/Instruments/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GridItem from '../GridItem';
-import Table from '@/app/trading/(components)/Instruments/Table';
+import Table from './Table';
 
 const Instruments = () => {
   return <GridItem title="Instruments" content={<Table />} />;
